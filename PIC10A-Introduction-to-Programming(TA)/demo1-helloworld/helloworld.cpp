8acbc0ceba9ac38d8ff76dddb69114bc66392c17
evastgh <evast@g.ucla.edu>
2017-04-05 14:04:00 -0700

    pic10a demo1

--- PIC10A-Introduction-to-Programming(TA)/demo1-helloworld/helloworld.cpp
+++ PIC10A-Introduction-to-Programming(TA)/demo1-helloworld/helloworld.cpp
@@ -1,10 +1,11 @@
 #include <iostream> // include the library that contains the methods you need, for instance, " cout" and "endl"
 
-using namespace std; // ALWAYS PUT THIS IN THE FIRST LINE
+using namespace std; // JUST ALWAYS PUT THIS IN THE FIRST LINE
 int main(){
 	/* cout << */ 
 	/* cout << 1/0 << endl; */
 	cout << "Hello world!" << endl;
 	/* cout << "Hel\nlo world!" << endl; */
-	return 0;
+	/* return 0; */ 
+	// check this out: https://stackoverflow.com/questions/22239/why-does-int-main-compile/
 }
