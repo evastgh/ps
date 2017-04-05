e5795b17ef5ab1abbda90c4dbe3ea6f63f903db2
evastgh <evast@g.ucla.edu>
2017-04-05 12:44:38 -0700

    pic10a demo1

---
+++ PIC10A-Introduction-to-Programming(TA)/demo1/helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // include the library that contains the methods you need, for instance, " cout" and "endl"
+
+using namespace std; // ALWAYS PUT THIS IN THE FIRST LINE
+int main(){
+	/* cout << */ 
+	cout << 1/0 << endl;
+	/* cout << "Hello world!" << endl; */
+	/* cout << "Hel\nlo world!" << endl; */
+	return 0;
+}
