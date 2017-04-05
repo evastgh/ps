6daea44ed657e1ef7f241c6f39c38feed91d01a3
Stephanie Wang <srw@math.ucla.edu>
2017-04-05 13:05:32 -0700

    rename file

--- PIC10A-Introduction-to-Programming(TA)/demo1-helloworld/helloworld.cpp
+++ PIC10A-Introduction-to-Programming(TA)/demo1-helloworld/helloworld.cpp
@@ -3,8 +3,8 @@
 using namespace std; // ALWAYS PUT THIS IN THE FIRST LINE
 int main(){
 	/* cout << */ 
-	cout << 1/0 << endl;
-	/* cout << "Hello world!" << endl; */
+	/* cout << 1/0 << endl; */
+	cout << "Hello world!" << endl;
 	/* cout << "Hel\nlo world!" << endl; */
 	return 0;
 }
