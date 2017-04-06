d8c93c32060610d7455db3be9be6da8aae440e38
evastgh <evast@g.ucla.edu>
2017-04-06 09:55:14 -0700

    2nd discussion

--- PIC10A-Introduction-to-Programming(TA)/demo1-helloworld/helloworld.cpp
+++ PIC10A-Introduction-to-Programming(TA)/demo1-helloworld/helloworld.cpp
@@ -1,11 +1,27 @@
-#include <iostream> // include the library that contains the methods you need, for instance, " cout" and "endl"
+#include <iostream> // include the library that contains the methods you need, for instance, "cout" and "cin"
 
-using namespace std; // JUST ALWAYS PUT THIS IN THE FIRST LINE
-int main(){
-	/* cout << */ 
-	/* cout << 1/0 << endl; */
-	cout << "Hello world!" << endl;
-	/* cout << "Hel\nlo world!" << endl; */
-	/* return 0; */ 
+using namespace std; // specify the standard namespace
+int main() {
+	cout << "Hello, world!\n"; // display greeting and append new line
+	cout << "How are you?"; // display question
+
+	cout << "Hel\nlo world!"; // '\n' can be placed anywhere in the string
+
+
+ 	// cout <<    // this line will cause compiler error 
+	// cout <<	; // this line will cause compiler error
+	cout << "Hello world!";; // this line doesn't cuase compiler error
+	// count << "Hello world!"; // "cout" stands for "Console OUTput". Misspelling the command causes compiler error
+
+	cout << ""; // empty string, no compiler error
+
+	cout << "Hello, world!\n" << "How are you?"; // stack multiple strings into cout
+	
+	cout << "Hello world! \\(^_^)/\n"; // solution to in class little challenge
+
+	cin.get(); // line added so the program pauses until the user hits ENTER
+	// system("pause"); // an alternative for cin.get();
+	
+	return 0; // terminate program
 	// check this out: https://stackoverflow.com/questions/22239/why-does-int-main-compile/
 }
