81fcb3f2d96251758c35dc101e258021bc0ab9d1
evastgh <evast@g.ucla.edu>
2017-04-18 07:53:32 -0700

    more floating point number testing

--- PIC10A_Introduction_to_Programming_TA/demo5_limit_of_int/limit_of_int.cpp
+++ PIC10A_Introduction_to_Programming_TA/demo5_limit_of_int/limit_of_int.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 int main(){
-    int LIMIT_OF_INT = 100;
-    bool notCorrectYet = true;
-    while (notCorrectYet){
-	LIMIT_OF_INT = LIMIT_OF_INT + 1;
-	if (LIMIT_OF_INT >= LIMIT_OF_INT + 1){
-	    notCorrectYet = false; // it's correct NOW
-	}
-    }
-    cout << "The limit of type INT is " << LIMIT_OF_INT << endl;
+    // int LIMIT_OF_INT = 100;
+    // bool notCorrectYet = true;
+    // while (notCorrectYet){
+	// LIMIT_OF_INT = LIMIT_OF_INT + 1;
+	// if (LIMIT_OF_INT >= LIMIT_OF_INT + 1){
+	    // notCorrectYet = false; // it's correct NOW
+	// }
+    // }
+    // cout << "The limit of type INT is " << LIMIT_OF_INT << endl;
 
     // cout << 2147483647 + 1 << endl;
     // cout << ++LIMIT_OF_INT << endl;
     // cout << LIMIT_OF_INT - 1 << endl;
+    
+    // double LIMIT_OF_DOUBLE = 1e-4;
+    // bool stop = false;
+    // while (!stop) {
+	// cout << "LIMIT_OF_DOUBLE = " << LIMIT_OF_DOUBLE << endl;
+	// cout << "LIMIT_OF_DOUBLE + 1 = " << LIMIT_OF_DOUBLE + 1 << endl;
+	// LIMIT_OF_DOUBLE /= 3.;
+	// cin >> stop;
+    // }
+    // cout << 1 + 0.100001 << endl;
 }
