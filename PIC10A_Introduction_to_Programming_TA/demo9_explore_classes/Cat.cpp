114f75b8e65bc79e124f8eeeef0ea6150af334aa
evastgh <evast@g.ucla.edu>
2017-04-27 08:53:56 -0700

    demo9 classes updated

--- PIC10A_Introduction_to_Programming_TA/demo9_explore_classes/Cat.cpp
+++ PIC10A_Introduction_to_Programming_TA/demo9_explore_classes/Cat.cpp
@@ -5,15 +5,10 @@ Cat::Cat() {
 	setBreed("American Shorthair"); // default breed
 }
 
-// Cat::Cat(std::string breed_in) {
-// 	setBreed(breed_in);
-// }
-
 void Cat::setBreed(std::string breed_in) {
 	breed = breed_in;
 }
 
-// std::string Cat::talk() const {
 std::string Cat::talk(int number) {
 	age = age + number;
 	return "meow";
