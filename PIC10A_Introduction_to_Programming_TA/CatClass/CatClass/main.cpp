19c53b226a0d3887109c9500a24599e3933d497d
evastgh <evast@g.ucla.edu>
2017-05-01 10:54:55 -0700

    organize

--- PIC10A_Introduction_to_Programming_TA/CatClass/CatClass/main.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include "Cat.hpp"
-using namespace std;
-int main() {
-    Cat fluffy; // create a Cat called fluffy
-    cout << "Fluffy is a " << fluffy.breed << endl;
-    cout << "Fluffy says: "<< fluffy.talk() << endl; // output cat's meow
-    return 0; // terminate program
-}
