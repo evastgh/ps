9056168ec25cba98fb15c3deba804b2f862a0ae6
Stephanie Wang <evast@g.ucla.edu>
2018-05-10 10:58:39 -0700

    ....Q1

--- PIC10C_Advanced_Programming_TA/week7_smart_ptr/midtermQ1.cpp
+++ PIC10C_Advanced_Programming_TA/week7_smart_ptr/midtermQ1.cpp
@@ -4,17 +4,18 @@ class Algo {       // Algo = Something in Spanish
     private:
         int* theData;
         size_t theSize;
+        std::string name;
     public:
-        Algo( size_t n = 1 ) : theSize(n), theData( new int[n] ) { std::cout << "Algo constructor: theData = " << theData << std::endl; }
+        explicit Algo(const std::string& name = "default", size_t n = 1) : theSize(n), name(name), theData( new int[n] ) { std::cout << "Algo constructor: name = " << name << " theData = " << theData << std::endl; }
         // read https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.3.0/com.ibm.zos.v2r3.cbclx01/explicit_keyword.htm
-        /*explicit*/ Algo( const Algo& b ) : theSize( b.theSize ) {
+        /*explicit*/ Algo( const Algo& b ) : theSize( b.theSize ) , name(b.name + "_copy") {
             theData = new int[theSize];
             for ( size_t i = 0 ; i < theSize ; ++i )
                 theData[i] = b.theData[i];
-            std::cout << "Algo copy constructor: theData = " << theData << std::endl; 
+            std::cout << "Algo copy constructor: name = " << name << " theData = " << theData << std::endl; 
         }
-        void saySomething() const { std::cout << "Algo saySomething: theData = " << theData << std::endl; }
-        Algo& operator=( const Algo& rhs ) = delete;
+        void saySomething() const { std::cout << "Algo saySomething: name = " << name << " theData = " << theData << std::endl; }
+        // Algo& operator=( const Algo& rhs ) = delete;
         // Algo& operator=( const Algo& rhs ) {
         //     theSize = rhs.theSize;
         //     delete[] theData;
@@ -30,7 +31,7 @@ class Algo {       // Algo = Something in Spanish
 int main() {
     {
         // Algo thing1(3);
-        Algo thing2 = Algo();
+        Algo thing2 = Algo("temporary", 10);
         thing2.saySomething();
         // thing2 = Algo();
         // thing2.saySomething();
