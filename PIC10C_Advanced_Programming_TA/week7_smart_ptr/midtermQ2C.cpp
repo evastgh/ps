a839c93b8e75e0d4b297b2bb2c38b15c47a930fd
Stephanie R. Wang <evast@g.ucla.edu>
2018-05-10 11:20:30 -0700

    add suggested readings RE void argument

--- PIC10C_Advanced_Programming_TA/week7_smart_ptr/midtermQ2C.cpp
+++ PIC10C_Advanced_Programming_TA/week7_smart_ptr/midtermQ2C.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-void* (*non_void)(void,void*);
+// read this https://softwareengineering.stackexchange.com/questions/286490/what-is-the-difference-between-function-and-functionvoid
+void* (*non_void)(void*,void);
 
 void* (*non_void2)(void);
 
