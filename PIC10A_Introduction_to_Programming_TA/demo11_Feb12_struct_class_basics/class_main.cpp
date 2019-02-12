70941b25fd766afaa10c05f7156cf058a0c3fd72
Stephanie R. Wang <7941538+evastgh@users.noreply.github.com>
2019-02-12 14:47:56 -0800

    Create class_main.cpp

---
+++ PIC10A_Introduction_to_Programming_TA/demo11_Feb12_struct_class_basics/class_main.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include "Person.h"
+
+using namespace std;
+int main()
+{
+	Person person1, person2, person3;
+	cout << "Please input the first person's name, phone number, favorite dessert, separated by new line character." << endl;
+	person1.set_name();
+	person1.set_phone_number();
+	person1.set_favorite_dessert();
+	person1.print_status();
+    return 0;
+}
+
